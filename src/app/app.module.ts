ad92ed3489ccf6c8055dd15c531cb34bf5618692
Evance23 <evancebarracks01@gmail.com>
2021-03-29 13:44:22 +0300

    Add modified files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FormsComponent } from './forms/forms.component';
+import { WisdomComponent } from './wisdom/wisdom.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    FormsComponent
+    FormsComponent,
+    WisdomComponent
   ],
   imports: [
     BrowserModule,
