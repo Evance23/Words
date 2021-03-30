a2732f6a0d04828f0046de6f25de424ebe52d0d7
Evance23 <evancebarracks01@gmail.com>
2021-03-30 12:10:04 +0300

    Add changes

--- src/app/forms/forms.component.ts
+++ src/app/forms/forms.component.ts
@@ -1,31 +1,49 @@
 import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 import { Words } from '../words';
+
+
 @Component({
   selector: 'app-forms',
   templateUrl: './forms.component.html',
   styleUrls: ['./forms.component.css']
 })
-export class FormssubmitQuote(){
-  // this.addQuote.emit(this.newQuote);
-  // this.newQuote = new Words(0, ""," "," ");Component implements OnInit {
 
-    newQuote = new Words(0, ""," "," ");
-    @Output() addQuote = new EventEmitter<Words>();
 
-    }
+export class FormsComponent implements OnInit {
+//   this.addQuote.emit(this.newQuote);
+newQuote = new Words(0, "", " ", " ");
+constructor(){
+
+}
+ngOnInit(){
+  
+}
+
+
+}
+
+
+
+
+
+// newQuote = new Words(0, "", " ", " ");
+// @Output() addQuote = new EventEmitter<Words>();
+
+
+
 
 //   submitQuote(){
 // this.addQuote.emit(this.newQuote);
 // this.newQuote = new Words(0, ""," "," ");
 
-}
-    
-    constructor() { }
-  
-    ngOnInit() {
-    }
-  
-  }
-  
+
+    // constructor() {
+
+
+
+    // ngOnInit() {
+    // }
+
+
 
 
