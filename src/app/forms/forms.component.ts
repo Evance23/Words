3a0f54d23050ea204ade8657265cb66b947da913
Evance23 <evancebarracks01@gmail.com>
2021-03-30 17:53:41 +0300

    Add all changes

--- src/app/forms/forms.component.ts
+++ src/app/forms/forms.component.ts
@@ -10,16 +10,10 @@ import { Words } from '../words';
 
 
 export class FormsComponent implements OnInit {
-  newQuotes=[]
-  quotes:Words[]=[
-
-  new Words(1, 'Fair is foul and foul is fair.', 'Bertolt ', 0,0,"5/5/2021"),
-  // new Words(2, 'If you talk to him in his language, that goes to his heart ', 'Nelson Mandela', 0,0, "5/5/2021"),
-  // new Words(3, 'Whether you think you can or think you cant, you are right.', 'Anonymous', 0,0, "5/5/2021")
-];
-//   this.addQuote.emit(this.newQuote);
+  newQuotes=[] as any
+  
 newQuote = new Words(0, "", " ", 0,0 , " ");
-
+@Output() createdQuote = new EventEmitter<any>();
 constructor(){
 
 }
@@ -27,5 +21,7 @@ ngOnInit(){
   
 }
 
-
+addQuotes(value:any){
+  this.createdQuote.emit(value)
+}
 }
--- src/app/wisdom/wisdom.component.ts
+++ src/app/wisdom/wisdom.component.ts
@@ -7,41 +7,39 @@ import { Words } from '../words';
 })
 
 export class WisdomComponent implements OnInit {
+  
   quotes:Words[]=[
-
-      new Words(1, 'Fair is foul and foul is fair.', 'Bertolt ', 0,0,"5/5/2021"),
-      // new Words(2, 'If you talk to him in his language, that goes to his heart ', 'Nelson Mandela', 0,0, "5/5/2021"),
-      // new Words(3, 'Whether you think you can or think you cant, you are right.', 'Anonymous', 0,0, "5/5/2021")
+    new Words(1, 'Fair is foul and foul is fair.', 'Bertolt ', 0,0,"5/5/2021"),
+    new Words(2, 'If you talk to him in his language, that goes to his heart ', 'Nelson Mandela', 0,0, "5/5/2021"),
   ];
 
   constructor(){
-    //this.quotes=['Fair is foul and foul is fair', 'Whether you think you can or think you cant, you are right', '“If you talk to a man in a language he understands, that goes to his head. If you talk to him in his language, that goes to his heart.-Nelson Mandela']
-  
   
   }
+
   ngOnInit(){
-    // this.quotes=  [
-    //   { id: 1,
-    //     author:"Bertolt",
-    // date: "5/5/2021",
-    // quote:"Fair is foul and foul is fair."},
-    // { id: 2,
-    //   author: "Nelson Mandela",
-    // date: "5/5/2021",
-    // quote:"If you talk to a man in a language he understands, that goes to his head. If you talk to him in his language, that goes to his heart"},
-    // { id: 3,
-    //   author: "Anonymous",
-    // date:"5/5/2021",
-    // quote:"Whether you think you can or think you cant, you are right"}]
+    
+  }
+ 
+  recievedQuote(value:any){
+    let id = this.quotes.length + 1
+    value.id = id
+    this.quotes.push(value)
   }
 
-  addNewQuote(quotes:Words){
-    let quotesLength = this.quotes.length;
-    quotes.id = quotesLength+1;
-    // quotes.date = new Date(quotes.date)
-    
-    this.quotes.push(quotes)
-}
+  upvote(quote:Words){
+    quote.upvote += 1
+  }
 
+  downvote(quote:Words){
+    quote.downvote += 1
+  }
+
+  deleteQuote(quote:Words){
+    let toDelete = this.quotes.indexOf(quote)
+    if (toDelete > -1) {
+      this.quotes.splice(toDelete)
+    }
+  }
 
 }
